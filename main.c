c4c3b7ac357a74eda0441b73bff27e2adfe8097c
Seekfall <seekfallsec@gmail.com>
2025-11-12 09:20:28 +0100

    ajouts d'un check sur curl + modification gitignore

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <stdlib.h>
 
 int main(void) {
     curl_version_info_data *vinfo = curl_version_info(CURLVERSION_NOW);
     printf("libcurl version: %s\n", vinfo->version);
     return 0;
+}
+
+
+
+int heartbeat(){
+    CURL *curl;
+    CURLcode res;
+
+    /*Inits the winsock*/
+    res=curl_global_init(CURL_GLOBAL_DEFAULT);
+    
+    if(res!=CURLE_OK){
+        fprintf(stderr,"curl_global_init() failed: %s\n",curl_easy_strerror(res));
+        exit(EXIT_FAILURE);
+    }
+
+    
 }
